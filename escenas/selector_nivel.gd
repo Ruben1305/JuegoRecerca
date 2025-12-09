6a1730ced1e0fdb532fd7b40fcf057c819b00a4e
Ruben1305 <alcantarillagomezruben@elpuig.xeill.net>
2025-12-09 13:20:00 +0100

    Mikol

--- escenas/selector_nivel.gd
+++ escenas/selector_nivel.gd
@@ -5,19 +5,7 @@ func _ready() -> void:
 	get_tree().paused = false #Aqui como xiste el menu de pausa pues lo despausamos
 
 func _on_lvl_1_pressed() -> void:
-	get_tree().change_scene_to_file("res://escenas/Inicio.tscn")
+	get_tree().change_scene_to_file("res://escenas/LVL'S/game1.tscn")
 
 func _on_lvl_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://escenas/SelectorNivel.tscn")
-
-func _on_lvl_3_pressed() -> void:
-	get_tree().change_scene_to_file("res://escenas/SelectorNivel.tscn")
-
-func _on_lvl_4_pressed() -> void:
-	get_tree().change_scene_to_file("res://escenas/SelectorNivel.tscn")
-
-func _on_lvl_5_pressed() -> void:
-	get_tree().change_scene_to_file("res://escenas/SelectorNivel.tscn")
-
-func _on_lvl_6_pressed() -> void:
-	get_tree().change_scene_to_file("res://escenas/SelectorNivel.tscn")
+	get_tree().change_scene_to_file("res://escenas/Inicio.tscn")

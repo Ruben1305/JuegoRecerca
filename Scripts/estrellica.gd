b67b691433939e3d346d8d59c67f48c99f0bd874
Ruben1305 <alcantarillagomezruben@elpuig.xeill.net>
2026-02-02 10:49:28 +0100

    Estrella sound

--- Scripts/estrellica.gd
+++ Scripts/estrellica.gd
@@ -4,22 +4,35 @@ extends Area3D
 @export var float_amplitude: float = 0.5
 @export var float_speed: float = 2.0
 
+@onready var sonido: AudioStreamPlayer3D = $AudioStreamPlayer3D
+@onready var collision: CollisionShape3D = $CollisionShape3D
+
 var base_y: float
 var time: float = 0.0
 
 func _ready():
 	base_y = global_position.y
-	# Conectamos la señal aquí para evitar errores
 	body_entered.connect(_on_estrellita_body_entered)
+	print(sonido)
 
 func _process(delta):
-	# Rotación
 	rotation_degrees.y += rotation_speed * delta
-	# Flotación
 	time += delta
 	global_position.y = base_y + sin(time * float_speed) * float_amplitude
 
-# Se ejecuta cuando el jugador entra en el área
 func _on_estrellita_body_entered(body: Node) -> void:
-	if body.is_in_group("player"):
-		queue_free()             # desaparece la estrella
+	if not body.is_in_group("player"):
+		return
+
+	if sonido:
+		sonido.play()
+
+	# Oculta y deshabilita colisión para evitar recoger varias veces
+	hide()
+	collision.disabled = true
+
+	# Espera a que termine el sonido antes de eliminar el nodo
+	if sonido:
+		await sonido.finished
+
+	queue_free()
